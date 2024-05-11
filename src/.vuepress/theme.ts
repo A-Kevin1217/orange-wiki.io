c2546d955e6d25434b84f62c1c7697b09a0abdb6
小丞 <jiyucheng2001@foxmail.com>
2024-05-11 22:08:59 +0800

    Update theme.ts

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -28,7 +28,6 @@ export default hopeTheme({
   footer: "请勿商用，违者必究",
   displayFooter: false,
 
-  plugins.sitemap: false,
   // 加密配置
   encrypt: {
     config: {
@@ -41,6 +40,8 @@ export default hopeTheme({
 
   // 在这里配置主题提供的插件
   plugins: {
+
+    sitemap: false,
     // 注意: 仅用于测试! 你必须自行生成并在生产环境中使用自己的评论服务
     comment: {
       provider: "Giscus",
