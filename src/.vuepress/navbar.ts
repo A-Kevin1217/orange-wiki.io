905da656d41132e915c3cc3cf79c8f4e6d4733c1
小丞 <jiyucheng2001@foxmail.com>
2025-08-14 00:50:36 +0800

    docs: 更新导航结构，新增“其他”分类及相关链接
    在导航中重新组织了结构，新增“其他”分类，包含“公屏使用指南”和“按钮指南”链接，同时将“光遇相关”下的内容进行了细分，提升了导航的清晰度和用户体验。

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -18,42 +18,78 @@ export default navbar([
         icon: "mynaui:terminal-solid",
         prefix: "more/",
         children: [""],
-      },
-      {
-        text: "公屏使用指南",
-        icon: "tabler:speakerphone",
-        prefix: "awesome",
-        link: "awesome",
-      },
+      },{
+        text: "其他",
+        icon: "ri:image-2-line",
+        children: [
+          {
+            text: "公屏使用指南",
+            icon: "tabler:speakerphone",
+            prefix: "awesome",
+            link: "awesome",
+          },
+          {
+            text: "按钮指南",
+            icon: "game-icons:button-finger",
+            link: "BUG.html",
+          },
+        ],
+      }
     ],
   },
+  
   {
-    text: "按钮指南",
-    icon: "game-icons:button-finger",
-    link: "BUG.html",
-  },
-  {
-    text: "光遇身高查询",
+    text: "光遇相关",
     icon: "arcticons:sky",
     prefix: "/height/",
     children: [
       {
-        text: "三服身高查询教程",
-        icon: "tabler:book-filled",
-        link: "more/",
-      },
-      {
-        text: "免费查身高",
-        icon: "pixelarticons:human-height",
-        link: "http://sky.kevin-study.top/",
-      },
+        text: "身高相关",
+        icon: "mynaui:user-height",
+        children: [
+          {
+            text: "三服身高查询教程",
+            icon: "tabler:book-filled",
+            link: "more/",
+          },
+          {
+            text: "免费查身高",
+            icon: "pixelarticons:human-height",
+            link: "http://sky.kevin-study.top/",
+          },
+        ],
+      },{
+        text: "其他",
+        icon: "ri:image-2-line",
+        children: [
+          {
+            text: "光遇养成价格计算",
+            icon: "arcticons:sky",
+            link: "瞎几把算.html",
+          }
+        ],
+      }
     ],
   },
   {
-    text: "光遇养成价格计算",
-    icon: "arcticons:sky",
-    link: "瞎几把算.html",
+    text: "Minecraft",
+    icon: "arcticons:minecraft",
+    prefix: "/mc/",
+    children: [
+      {
+        text: "Y.C.L World",
+        icon: "mynaui:server",
+        children: [
+          {
+            text: "服务器相关指令",
+            icon: "mynaui:terminal-solid",
+            link: "Y.C.L常用指令手册.html",
+          }
+        ],
+      }
+    ],
   },
+  
   {
     text: "友情链接",
     icon: "ri:links-line",
---
+++ src/mc/README.md
@@ -0,0 +1,13 @@
+---
+ title: Minecraft|我的世界
+ icon: gamepad
+---
+
+ <br>
+ <iframe style="width:728px;height:90px;max-width:100%;border:none;display:block;margin:auto" src="https://namemc.com/server/play.simpfun.cn:35595/embed" width="728" height="90"></iframe>
+
+ 
+ ## Minecraft指令
+
+- [Y.C.L常用指令手册](Y.C.L常用指令手册.html)
+
